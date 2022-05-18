666b4b134d0cf6804515119ebc3088779143e136
SibylGuo <89446073+SibylGuo@users.noreply.github.com>
2022-05-19 08:29:09 +1000

    html

---
+++ Green_Apple_Books/JS/main.js
@@ -0,0 +1,20 @@
+function buttonClick(buttonElement){
+    alert("hey you cliced"+buttonElement.textconContent
+    );
+    console.log("hey you clicked"+buttonElement.textconContent);
+
+}
+
+window.addEventListener("DOMContentLoaded",function(){
+    var buttonslist = document.getElementsByClassName("button");
+});
+    for(const button of buttonslist){button.addEventListener("click", function(event); )}
+
+const getData = (pageUrl){
+    return new Promise((resolve, reject)
+        let request = new XMLHttpRequest();
+        request.onload = functionn(){
+            if (this.readyState)
+        }
+    })
+}
