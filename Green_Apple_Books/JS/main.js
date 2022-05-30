4fde871d108d124784e2bb33a192346a4de5962d
SibylGuo <89446073+SibylGuo@users.noreply.github.com>
2022-05-30 22:23:00 +1000

    nav bar

--- Green_Apple_Books/JS/main.js
+++ Green_Apple_Books/JS/main.js
@@ -1,20 +1,4 @@
-function buttonClick(buttonElement){
-    alert("hey you cliced"+buttonElement.textconContent
-    );
-    console.log("hey you clicked"+buttonElement.textconContent);
-
-}
-
-window.addEventListener("DOMContentLoaded",function(){
-    var buttonslist = document.getElementsByClassName("button");
-});
-    for(const button of buttonslist){button.addEventListener("click", function(event); )}
-
-const getData = (pageUrl){
-    return new Promise((resolve, reject)
-        let request = new XMLHttpRequest();
-        request.onload = functionn(){
-            if (this.readyState)
-        }
-    })
+function toggleResponsive(){
+    var navbar = document.getElementById("topNav");
+    navbar.classList.toggle("responsive");
 }
