1904518d39cdaf4c399cf13736f3c580ad486ec3
SibylGuo <89446073+SibylGuo@users.noreply.github.com>
2022-06-01 23:45:12 +1000

    checkout

--- Green_Apple_Books/JS/main.js
+++ Green_Apple_Books/JS/main.js
@@ -2,3 +2,71 @@ function toggleResponsive(){
     var navbar = document.getElementById("topNav");
     navbar.classList.toggle("responsive");
 }
+
+function toggledrop1(){
+    var  drop1= document.getElementById("relatedEditions");
+    drop1.classList.toggle("drop");
+}
+
+function toggledrop2(){
+    var  drop2 = document.getElementById("storeAvail");
+    drop2.classList.toggle("drop");
+}
+
+function toggledrop3(){
+    var  drop3= document.getElementById("description");
+    drop3.classList.toggle("drop");
+}
+
+function toggledrop4(){
+    var  drop4 = document.getElementById("aboutAuthor");
+    drop4.classList.toggle("drop");
+}
+
+function toggledrop5(){
+    var  drop5= document.getElementById("review");
+    drop5.classList.toggle("drop");
+}
+
+function toggledrop6(){
+    var  drop6 = document.getElementById("productDetails");
+    drop6.classList.toggle("drop");
+}
+
+function toggledrop7(){
+    var  drop7 = document.getElementById("categories");
+    drop7.classList.toggle("drop");
+}
+
+function toggledropinfo1(){
+    var  dropinfo1 = document.getElementById("storeSpecials");
+    dropinfo1.classList.toggle("drop");}
+
+
+function toggledropinfo2(){
+    var  dropinfo2 = document.getElementById("customerInfo");
+    dropinfo2.classList.toggle("drop");}
+
+function toggledropinfo3(){
+    var  dropinfo3 = document.getElementById("pickUp");
+    dropinfo3.classList.toggle("drop");}
+
+function toggledropinfo4(){
+    var  dropinfo4 = document.getElementById("delivery");
+    dropinfo4.classList.toggle("drop");}
+
+function toggledropinfo5(){
+    var  dropinfo5 = document.getElementById("shipAvailability");
+    dropinfo5.classList.toggle("drop");}
+
+function toggledropinfo6(){
+    var  dropinfo6 = document.getElementById("shippingCost");
+    dropinfo6.classList.toggle("drop");}
+
+function toggledropinfo7(){
+    var  dropinfo7 = document.getElementById("payment");
+    dropinfo7.classList.toggle("drop");}
+
+function toggledropinfo8(){
+    var  dropinfo8 = document.getElementById("comments");
+    dropinfo8.classList.toggle("drop");}
